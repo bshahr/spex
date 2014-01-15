4c44ba83d884bf603c2668ddd01825a44acf4a27
Ziyu Wang <ziyucwang@gmail.com>
2014-01-15 16:03:48 +0000

    finished duplicat.py

--- duplicate.py
+++ duplicate.py
@@ -3,6 +3,40 @@ import distutils.core
 import re
 import random
 
+def write_PBS_script(folder, serial, num, path):
+	content = """
+#!/bin/bash
+#PBS -S /bin/bash
+
+#!/bin/sh
+# Script for running serial program, diffuse.
+
+#PBS -l walltime=02:00:00
+#PBS -l mem=2000mb
+#PBS -t 1-{}
+#PBS -r n
+#PBS -M ziyucwang@gmail.com
+#PBS -m bea
+#PBS -V
+#PBS -l nodes=1:ppn=4
+
+cd $PBS_O_WORKDIR
+echo "Current working directory is `pwd`"
+echo PBS_ARRAYID=$PBS_ARRAYID
+# Run
+echo "Starting run: $(date)"
+/home/ziyuw/projects/expSpearmint/job_scripts/runscript.sh {} {} $PBS_ARRAYID
+echo "Run complete: $(date)"
+""".format(num, folder, serial)
+	
+	fname = path + 'job_scripts_{}.pbs'.format(serial)
+	f = open(fname, 'w')
+	f.write(content)
+	f.close()
+
+	return fname
+
+
 def copyFolder(name='braninpy', num=20):
 
 	def getPath(subfolder):
@@ -22,8 +56,18 @@ def copyFolder(name='braninpy', num=20):
 		distutils.dir_util.copy_tree(origPath, destPath)
 
 
-	print 'serial:', serial
+	fname = write_PBS_script(name, serial, num, getPath('job_scripts/'))
+
+
+	execute = 'qsub -l walltime=5:00:00, mem=2gb {}'.format(fname)
+	print execute
+
 	return serial
 
 if __name__ == '__main__':
-	copyFolder()
+	copyFolder('braninpy', 20)
+	# copyFolder('hart3py', 20)
+	# copyFolder('hart6py', 20)
+
+	
+	
