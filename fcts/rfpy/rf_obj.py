5183cbbaaac1a22a91aa74aadefaf671b185c94a
Ziyu Wang <ziyucwang@gmail.com>
2014-01-21 20:32:05 +0000

    modified objective to fix error.

--- fcts/rfpy/rf_obj.py
+++ fcts/rfpy/rf_obj.py
@@ -9,25 +9,25 @@ def convert(params):
         '/home/ziyuw/projects/jointtracking_ziyu/depth_to_bodyparts/'
     
     padding = np.array([500000, 0.001, 100, 0.001, 100])
-    params = np.hstack([params[0], padding, params[1:]])
+    params = np.hstack([padding, params[1:]])
 
-    param_dict = {'key{}'.format(key): value for \
+    param_dict = {'key{}'.format(key+1): value for \
         (key, value) in enumerate(params)}
     param_dict['dataset_name'] = '1000_dataset_1000000'
     param_dict['path'] = path
     
     
-    discrete_indices = ['key0', 'key1', 'key3', 'key5', 'key6', 'key7', 'key8']
+    discrete_indices = ['key1', 'key3', 'key5', 'key6', 'key7', 'key8']
     for key in discrete_indices:
         param_dict[key] = int(param_dict[key])
 
-    return 'python {path}pipeline/02_train_class_trees.py -i {path}data/{dataset_name}/02_bundled_train/ -v {path}data/{dataset_name}/02_bundled_test/ -o ../data/1000_dataset_1000000/03_tree/ -j 2 -c 5 -n 50 -b {key0} -S {key1} -rs {key2} -ms {key3} -rt {key4} -mt {key5} -g 0 -d {key6} -s {key7} -t {key8} -umeanx {key9} -umeany {key10} -uvarx {key11} -uvary {key12} -uvarxy {key13} -vmeanx {key14} -vmeany {key15} -vvarx {key16} -vvary {key17} --vvarxy {key18}'.format(**param_dict)
+    return 'python {path}pipeline/02_train_class_trees.py -i {path}data/{dataset_name}/02_bundled_train/ -v {path}data/{dataset_name}/02_bundled_test/ -o ../data/1000_dataset_1000000/03_tree/ -j 2 -c 5 -n 50 -b 1 -S {key1} -rs {key2} -ms {key3} -rt {key4} -mt {key5} -g 0 -d {key6} -s {key7} -t {key8} -umeanx {key9} -umeany {key10} -uvarx {key11} -uvary {key12} -uvarxy {key13} -vmeanx {key14} -vmeany {key15} -vvarx {key16} -vvary {key17} --vvarxy {key18}'.format(**param_dict)
 
 def transform(params):
     mean_bound = 50.
-    param_bounds = \
-        np.array([[0, 1], \
-        [np.log(1.), np.log(60.)], \
+    param_bounds = np.array([\
+        # [0, 1], \
+        [np.log(1.), np.log(30.)], \
         [1., 100.], \
         [np.log(1.), np.log(5000.)], \
         [-mean_bound, mean_bound], \
@@ -40,8 +40,8 @@ def transform(params):
         [1.0, 200.0], \
         [1.0, 200.0], \
         [1.0, 200.0]])
-    discrete_indices = [0, 1, 2, 3]
-    log_indices = [1, 3]
+    discrete_indices = [0, 1, 2]
+    log_indices = [0, 2]
     
     params = params*(param_bounds[:, 1] - param_bounds[:, 0]) + \
         param_bounds[:, 0]
@@ -55,7 +55,11 @@ def transform(params):
 
 def call(x, path=None, arrayid=None):
     cmd = convert(transform(x))
-    result = float(subprocess.check_output(cmd.split()))
+    try:
+        result = float(subprocess.check_output(cmd.split()))
+    except:
+        cmd = '{} --max_node_count 1000000'.format(cmd)
+        result = float(subprocess.check_output(cmd.split()))
     if not path is None:
         f = open('{}record-{}.txt'.format(path, arrayid), 'a')
         f.write(cmd); f.write('\n')
