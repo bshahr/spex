7c66dfe4c0452018a6ebdd52f05f9db7080a053b
Ziyu Wang <ziyucwang@gmail.com>
2014-05-26 18:02:28 +0100

    Removed dependencies.
    Former-commit-id: fe5e5748d8e8917dce1dbff095617439fc65b9ae

--- fcts/braninpy/branin.py
+++ fcts/braninpy/branin.py
@@ -2,8 +2,17 @@ import numpy as np
 import sys
 import math
 import time
-from bo.demos.testFuncs import branin
 
+def branin(x):
+	"""
+		Branin function 
+		The number of variables n = 2.
+	"""
+	bound = np.array([[-5., 10.], [0., 15.]])
+	x = x*(bound[:, 1] - bound[:, 0]) + bound[:, 0]
+	y = (x[1]-(5.1/(4.0*np.pi**2))*x[0]**2.0 + \
+		5.0*x[0]/np.pi-6.0)**2.0+10.0*(1.0-1.0/(8.0*np.pi))*np.cos(x[0])+10.0;
+	return y
 
 # Write a function like this called 'main'
 def main(job_id, params):
