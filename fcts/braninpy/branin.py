076c914fbcb0ce47a88a71121823b65072ccba2e
Ziyu Wang <ziyucwang@gmail.com>
2014-05-27 22:32:36 +0100

    Bug fix.
    Former-commit-id: 934afffbe3c88bb5f3a6b836034d826a900d1b3b

--- fcts/braninpy/branin.py
+++ fcts/braninpy/branin.py
@@ -12,7 +12,7 @@ def branin(x):
 	x = x*(bound[:, 1] - bound[:, 0]) + bound[:, 0]
 	y = (x[1]-(5.1/(4.0*np.pi**2))*x[0]**2.0 + \
 		5.0*x[0]/np.pi-6.0)**2.0+10.0*(1.0-1.0/(8.0*np.pi))*np.cos(x[0])+10.0;
-	return y
+	return y/10.
 
 # Write a function like this called 'main'
 def main(job_id, params):
