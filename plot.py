40722e55d9d0e805b7b50607166b94218147c6a2
Bobak Shahriari <bshahr@cs.ubc.ca>
2014-10-14 14:20:11 -0700

    Added optional plotting configurations.
    Now you can configure the result of the `plot.py` script from an
    experiment's `config.yaml` file. All the following keywords are
    optional:
    
        color: str or hex, anything `pyplot` would understand;
        linestyle: str, also passed to `pyplot.{plot, semilogy}`;
        label: str, defaults to the config key of the particular method;
        order: number, used as a sorting key to maintain a desired
            legend order across experiments.

--- plot.py
+++ plot.py
@@ -11,9 +11,11 @@ matplotlib.use('pdf')
 
 import numpy as np
 import matplotlib.pyplot as pl
-import seaborn
+try: import seaborn
+except: pass
 
 import argparse
+import yaml
 import os.path
 import imp
 from jug import set_jugdir, value
@@ -32,10 +34,28 @@ set_jugdir(jugdata)
 execute_path = os.path.join(path, 'execute.py')
 data = imp.load_source('execute', execute_path).data
 
+# import plotting configurations
+with open(os.path.join(path, 'config.yaml')) as f:
+    config = yaml.safe_load(f)
+
 for function in data.keys():
     ax = pl.figure(1).gca()
     ax.cla()
-    for method in data[function].keys():
+
+    # sort keys in order requested by config file
+    keys = sorted(config.keys(), key=lambda k: config[k].get('order', -np.inf))
+
+    for method in keys:
+        c = config[method]
+
+        # load in plotting configurations
+        color = c.get('color', None)
+        linestyle = c.get('linestyle', '-')
+        label = c.get('label', method)
+        ymin = c.get('ymin', None)
+        log = False if (ymin is None) else c.get('log', False)
+
+        # manipulate data
         runs = np.array(value(data[function][method]))
         N = runs.shape[0]
         T = runs.shape[1]
@@ -43,13 +63,26 @@ for function in data.keys():
         y = runs.mean(axis=0)
         e = runs.std(axis=0) / np.sqrt(N) * 3
 
-        ax.plot(x, y, lw=2, label=method)
-        ax.fill_between(x, y-e, y+e, color=ax.lines[-1].get_color(), alpha=0.1)
+        if log:
+            if color is None:
+                ax.semilogy(x, y-ymin, lw=2, ls=linestyle, label=label)
+            else:
+                ax.semilogy(x, y-ymin, lw=2, color=color, ls=linestyle,
+                            label=label)
+            ax.fill_between(x, np.clip(y-e-ymin, 1e-10, np.inf), y+e-ymin,
+                            color=ax.lines[-1].get_color(), alpha=0.1)
+        else:
+            if color is None:
+                ax.plot(x, y, lw=2, ls=linestyle, label=label)
+            else:
+                ax.plot(x, y, lw=2, color=color, ls=linestyle, label=label)
+            ax.fill_between(x, y-e, y+e,
+                            color=ax.lines[-1].get_color(), alpha=0.1)
 
     ax.axis('tight')
     ax.axis(xmin=0, xmax=T)
     ax.set_xlabel('iterations')
-    ax.set_ylabel('function value')
+    ax.set_ylabel('absolute error' if log else 'function value')
     ax.legend(loc='best')
     ax.figure.canvas.draw()
 
@@ -59,6 +92,6 @@ for function in data.keys():
     if not os.path.isdir(figpath):
         os.mkdir(figpath)
     figpath = os.path.join(figpath, '{}.pdf'.format(function))
-    print figpath
     ax.figure.savefig(figpath, bbox_inches='tight')
+    print 'Plot saved in {}'.format(figpath)
 
