bb957e1cfb97ce9a7a8ea7ba5360d3125f5a6dad
Bobak Shahriari <bshahr@cs.ubc.ca>
2014-10-14 17:31:18 -0700

    Added median and quantile plotting.
    Simply appending the `--median` flag to the usual plotting command,
    i.e.
        python plot.py path/to/experiment --median
    
    plots the median and the middle 50% as a confidence band. Otherwise,
    the behaviour is almost the same and the mean +/- one standard error
    is plotted (this used to be +/- 3).

--- plot.py
+++ plot.py
@@ -23,6 +23,9 @@ from jug import set_jugdir, value
 # parse the path of the experiment to plot
 parser = argparse.ArgumentParser()
 parser.add_argument('path')
+parser.add_argument('--median', action='store_true', default=False,
+                    help='whether to plot the median and quantiles; ' +
+                    'defaults to False (i.e. mean and standard error).')
 args = parser.parse_args()
 
 # set where the data is stored
@@ -60,23 +63,32 @@ for function in data.keys():
         N = runs.shape[0]
         T = runs.shape[1]
         x = np.arange(1, T+1)
-        y = runs.mean(axis=0)
-        e = runs.std(axis=0) / np.sqrt(N) * 3
+        if args.median:
+            y = np.median(runs, axis=0)
+            ylo = np.percentile(runs, 25, axis=0)
+            yhi = np.percentile(runs, 75, axis=0)
+        else:
+            y = runs.mean(axis=0)
+            e = runs.std(axis=0) / np.sqrt(N)
+            ylo = y - e
+            yhi = y + e
 
+        # plotting
         if log:
             if color is None:
-                ax.semilogy(x, y-ymin, lw=2, ls=linestyle, label=label)
+                ax.semilogy(x, np.clip(y-ymin, 1e-8, np.inf),
+                            lw=2, ls=linestyle, label=label)
             else:
-                ax.semilogy(x, y-ymin, lw=2, color=color, ls=linestyle,
-                            label=label)
-            ax.fill_between(x, np.clip(y-e-ymin, 1e-10, np.inf), y+e-ymin,
+                ax.semilogy(x, np.clip(y-ymin, 1e-8, np.inf),
+                            lw=2, color=color, ls=linestyle, label=label)
+            ax.fill_between(x, np.clip(ylo-ymin, 1e-8, np.inf), yhi-ymin,
                             color=ax.lines[-1].get_color(), alpha=0.1)
         else:
             if color is None:
                 ax.plot(x, y, lw=2, ls=linestyle, label=label)
             else:
                 ax.plot(x, y, lw=2, color=color, ls=linestyle, label=label)
-            ax.fill_between(x, y-e, y+e,
+            ax.fill_between(x, ylo, yhi,
                             color=ax.lines[-1].get_color(), alpha=0.1)
 
     ax.axis('tight')
@@ -84,6 +96,7 @@ for function in data.keys():
     ax.set_xlabel('iterations')
     ax.set_ylabel('absolute error' if log else 'function value')
     ax.legend(loc='best')
+    ax.set_title(function)
     ax.figure.canvas.draw()
 
     # save figure
